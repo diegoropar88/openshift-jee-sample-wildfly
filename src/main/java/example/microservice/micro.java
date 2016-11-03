7d7c54a251f8b7af915313161639ba38abcf0493
sala-a <sala-a@11C01E14115955>
2016-11-02 20:17:54 -0500

    Update 3

--- src/main/java/example/microservice/micro.java
+++ src/main/java/example/microservice/micro.java
@@ -1,14 +1,21 @@
 package example.microservice;
 
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebService;
 
 @WebService(name="miServicio")
 public class micro {
 
 	@WebMethod()
-	public String sayHello(String name) {
+	public String sayHello(@WebParam(name="nombre") String name) {
 	    System.out.println("Hello: " + name);
 	    return "Hello " + name + "!";
+	}
+	
+	@WebMethod()
+	public int sumar(@WebParam(name="valor1") int a,@WebParam(name="valor2") int b) {
+	    System.out.println("sumar: " + (a+b));
+	    return (a+b);
 	}
 }
